01bcb2de0ff987874f846d446e50f1d799fdc992
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-10-15 14:52:51 +0100

    Adds all temperature functions to thermostat

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -6,13 +6,16 @@ describe("Thermostat", function() {
     thermostat = new Thermostat();
   });
 
-  describe('thermostat temperature', function() {
+  describe('new Thermostat', function() {
     it('initial temperature is 20 degrees', function() {
       expect(thermostat.current()).toEqual(20);
     });
+    it('power saving is on by default', function() {
+      expect(thermostat._isPowerSaving).toBe(true);
+    });
   });
 
-  describe('thermostat temperature can be increased', function() {
+  describe('.up', function() {
     it('temperature increases by 1 degree', function(){
       expect(thermostat.current()).toEqual(20);
       thermostat.up(1);
@@ -24,9 +27,22 @@ describe("Thermostat", function() {
       thermostat.up(5);
       expect(thermostat.current()).toEqual(25);
     });
+
+    it('temperature only goes to 25 degrees if power saving', function(){
+      expect(thermostat.current()).toEqual(20);
+      thermostat.up(10);
+      expect(thermostat.current()).toEqual(25);
+    });
+
+    it('temperature goes to 32 degrees if not power saving', function(){
+      expect(thermostat.current()).toEqual(20);
+      thermostat.togglePowerSavingOff();
+      thermostat.up(15);
+      expect(thermostat.current()).toEqual(32);
+    });
   });
 
-  describe('thermostat temperature can be decreased', function() {
+  describe('.down', function() {
     it('temperature decreases by 1 degree', function(){
       expect(thermostat.current()).toEqual(20);
       thermostat.down(1);
@@ -37,6 +53,43 @@ describe("Thermostat", function() {
       expect(thermostat.current()).toEqual(20);
       thermostat.down(3);
       expect(thermostat.current()).toEqual(17);
+    });
+
+    it('temp cannot be lowered below 10', function() {
+      thermostat.down(10);
+      thermostat.down(1);
+      expect(thermostat.current()).toEqual(10);
+    });
+
+    it('if down number makes tenp go below 10 temp will be 10', function() {
+      thermostat.down(15);
+      expect(thermostat.current()).toEqual(10);
+    });
+  });
+
+  describe('.reset', function() {
+    it('resets temp to 20 degrees', function() {
+      thermostat.down(5);
+      thermostat.reset();
+      expect(thermostat.current()).toEqual(20);
+    });
+  });
+
+  describe('.usage', function() {
+    it('reports low-usage for temps below 18', function() {
+      thermostat.down(3);
+      expect(thermostat.usage()).toEqual("low-usage");
+    });
+
+    it('reports medium-usage for temps between 18 and 24', function() {
+      thermostat.up(4);
+      expect(thermostat.usage()).toEqual("medium-usage");
+    });
+
+    it('reports high-usage for temps above 24', function() {
+      thermostat.togglePowerSavingOff();
+      thermostat.up(8);
+      expect(thermostat.usage()).toEqual("high-usage");
     });
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,15 +1,53 @@
 function Thermostat() {
   this._temp = 20;
+  this._isPowerSaving = true;
+  if(this._isPowerSaving === true) {
+    this._tempLimit = 25;
+  } else {
+    this._tempLimit = 32;
+  }
 }
 
 Thermostat.prototype.up = function(number) {
-  this._temp += number;
+  if(this._temp + number <= this._tempLimit) {
+    this._temp += number;
+  } else {
+    this._temp = this._tempLimit;
+  }
 }
 
 Thermostat.prototype.down = function(number) {
-  this._temp -= number;
+  if(this._temp - number >= 10) {
+    this._temp -= number;
+  } else {
+    this._temp = 10;
+  }
 }
 
 Thermostat.prototype.current = function() {
   return this._temp;
+}
+
+Thermostat.prototype.togglePowerSavingOff = function() {
+  this._isPowerSaving = false;
+  this._tempLimit = 32;
+}
+
+Thermostat.prototype.togglePowerSavingOn = function() {
+  this._isPowerSaving = true;
+  this._tempLimit = 25;
+}
+
+Thermostat.prototype.reset = function() {
+  this._temp = 20;
+}
+
+Thermostat.prototype.usage = function() {
+  if(this._temp < 18) {
+    return "low-usage";
+  } else if(this._temp >= 25) {
+    return "high-usage";
+  } else {
+    return "medium-usage";
+  }
 }
