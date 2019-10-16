537c5542c5d8a1e7505575e4b6d90b8e13775709
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-10-16 15:15:09 +0100

    Adds current weather in London

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemperature();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-weather').text(data.name + ": " + data.main.temp);
+  })
 
   $('#temperature-up').on('click', function() {
     thermostat.up(Number($("#amount").val()));
