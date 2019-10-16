d6fd57df0ee88027a335ceed081f816eaabffceb
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-10-16 15:50:00 +0100

    implemented Weather API

--- src/interface.js
+++ src/interface.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemperature();
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
-    $('#current-weather').text(data.name + ": " + data.main.temp);
+  displayWeather('london')
+
+
+  $('#city-submit').on('click', function() {
+    displayWeather($("#city").val())
   })
+
 
   $('#temperature-up').on('click', function() {
     thermostat.up(Number($("#amount").val()));
@@ -36,5 +40,14 @@ $(document).ready(function() {
     $('#power-usage').text(thermostat.usage());
     $('#temperature').attr('class', thermostat.usage());
     $('#style').attr('class', thermostat.usage());
+  }
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q='+city;
+    var appid = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var unit = '&units=metric';
+    $.get(url+appid+unit, function(data) {
+      $('#current-weather').text(data.name + ": " + data.main.temp);
+    })
   }
 })
